0e69ab5d2a02c83be9687ea7635db80a44754a40
jdm79 <james.malvern@gmail.com>
2022-07-20 22:41:43 +0100

    added timetamp, url, paper, body columns, works on local

--- app.py
+++ app.py
@@ -4,28 +4,39 @@ import psycopg2
 from bs4 import BeautifulSoup
 import database
 import requests
+import datetime
+
+# make a timestamp here or on PostgreSQL side for each headline row
+# https://stackoverflow.com/questions/38245025/how-to-insert-current-datetime-in-postgresql-insert-query
+# https://www.postgresql.org/docs/current/functions-datetime.html#FUNCTIONS-DATETIME-CURRENT
+# INSERT into "Group" (name,createddate) 
+# VALUES ('Test', current_timestamp);
 
 
 db_url = "postgres://oxbvadmp:stDxRRjicw60W6kRDQdDavS3g8_soU0Y@rogue.db.elephantsql.com/oxbvadmp"
 
 headers = {"Accept-language": "en-US, en;q=0.5"}
-guardian_url = "https://www.theguardian.com/uk"
+url = "https://www.theguardian.com/uk"
+paper = "The Guardian"
 
 # creates dynamic url to get the current day's list
-results = requests.get(guardian_url, headers=headers)
-
+results = requests.get(url, headers=headers)
 soup = BeautifulSoup(results.text, "html.parser")
 
+
+timestamp = '{:%b-%d-%Y %H:%M:%S}'.format(datetime.datetime.now())
 headline_html = soup.find('span', class_='js-headline-text')
 headline = headline_html.text.strip()
+# headline = "testing"
+body = "This is the body of the article."
 print(headline)
 
 connection = psycopg2.connect(db_url)
 # headline = 'Goodnight!'
 
+# database.add_columns(connection)
 database.create_tables(connection)
-database.create_tables(connection)
-database.add_headline(connection, headline)
+database.add_headline(connection, headline, url, paper, timestamp, body)
 
 
 
--- database.py
+++ database.py
@@ -2,18 +2,30 @@ from psycopg2.extras import execute_values
 
 CREATE_HEADLINES = """CREATE TABLE IF NOT EXISTS headlines 
 (headline TEXT);"""
-INSERT_HEADLINE = "INSERT INTO headlines (headline) VALUES (%s);"
+INSERT_HEADLINE = "INSERT INTO headlines (headline, url, paper, timestamp, body) VALUES (%s, %s, %s, %s, %s);"
 SELECT_ALL_HEADLINES = "SELECT * FROM headlines;"
+
+ADD_COLUMNS = """ALTER TABLE headlines
+ADD COLUMN url TEXT,
+ADD COLUMN paper TEXT,
+ADD COLUMN timestamp TEXT,
+ADD COLUMN body TEXT;"""
 
 def create_tables(connection):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(CREATE_HEADLINES)
 
-def add_headline(connection, headline):
+def add_columns(connection):
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(INSERT_HEADLINE, (headline,))        
+            cursor.execute(ADD_COLUMNS)
+
+
+def add_headline(connection, headline, url, paper, timestamp, body):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_HEADLINE, (headline, url, paper, timestamp, body))        
 
 def get_headlines(connection):
     with connection:
