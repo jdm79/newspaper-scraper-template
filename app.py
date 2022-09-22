523346d3113ee8c2a13c91a028d13b2fbeebc47c
jdm79 <james.malvern@gmail.com>
2022-09-22 14:39:01 +0100

    update irish sun html target

--- app.py
+++ app.py
@@ -10,18 +10,26 @@ import datetime
 
 db_url = "postgres://oxbvadmp:r5tJzuWI1-7XXKq1noJPVsoxFSQNfjG-@rogue.db.elephantsql.com/oxbvadmp"
 
-papers = [
-    [1, "https://www.mirror.co.uk/", "The Daily Mirror", '"h2", class_="story__title"'],
-    [2, "https://www.theguardian.com/uk", "The Guardian", "'span', class_='js-headline-text'"],
-    [3, "https://www.thesun.co.uk/", "The Sun", "'p', class_='teaser__subdeck'"],
-    [4, "https://www.ft.com/world/uk", "The Financial Times", "'div', class_='o-teaser__heading'"],
-    [5, "https://www.dailymail.co.uk/home/index.html", "The Daily Mail", "'h2', class_='linkro-darkred'"],
-    [6, "https://www.thetimes.co.uk/uk", "The Times", "'h3', class_='Headline--xl'"]
 
-]    
+fail = "Error - failed to scrape the text of this article from " 
+
+
+papers = [
+    [1, "https://www.mirror.co.uk/", "The Daily Mirror", "h2", "story__title"],
+    [2, "https://www.theguardian.com/uk", "The Guardian", "span", "js-headline-text"],
+    [3, "https://www.thesun.co.uk/", "The Sun", "p", "teaser__subdeck"],
+    [4, "https://www.ft.com/world/uk", "The Financial Times", "div", "o-teaser__heading"],
+    [5, "https://www.dailymail.co.uk/home/index.html", "The Daily Mail", "h2", "linkro-darkred"],
+    [6, "https://www.thetimes.co.uk/", "The Times", "h3", "Headline--xl"]
+
+]
+scrape_results = []
+
 
 def scrapeHeadlines():
-    scrape_results = []
+
+    connection = psycopg2.connect(db_url)
+
 
     randomUrls = [ 
     "https://www.facebook.com/", 
@@ -39,34 +47,29 @@ def scrapeHeadlines():
     id = paper[0]
     url = paper[1]
     newspaper = paper[2]
-    element_target = paper[3]
 
     results = requests.get(url, headers=headers)
-    soup = BeautifulSoup(results.text, "html.parser") # this can be printed out and works
-    # headline_html = soup.find(paper[3])
-    headline_html = soup.find(element_target) # this is not working like this
+    soup = BeautifulSoup(results.text, "html.parser")
+    headline_html = soup.find(paper[3], class_=paper[4])
 
     if headline_html != None:
       headline = headline_html.text.strip()
     else:
       headline = fail 
+
+    database.create_tables(connection)
+
+    database.add_headline(connection, headline, url, newspaper, timestamp)
     
-    scrape_results.append({
-                    'id': id,
-                    'paper': newspaper,
-                    'headline': headline,
-                    'headline_html': headline_html
-                    })
-    print(scrape_results)
+    # scrape_results.append({
+    #                 'id': id,
+    #                 'paper': newspaper,
+    #                 'headline': headline
+    #                 })
+
+
 
 for paper in papers:
     scrapeHeadlines()
 
-
-
-# connection = psycopg2.connect(db_url)
-
-# # database.add_columns(connection) ONLY USE THIS TO ADD NEW COLUMNS
-# # database.add_unique(connection)
-# database.create_tables(connection)
-# database.add_headline(connection, headline, url, paper, timestamp)
+print(scrape_results)
--- database.py
+++ database.py
@@ -1,13 +1,16 @@
 from psycopg2.extras import execute_values
 
+DROP_TABLE = "DROP TABLE IF EXISTS headlines;"
+
 CREATE_HEADLINES = """CREATE TABLE IF NOT EXISTS headlines (
     id SERIAL, 
-    headline TEXT UNIQUE,
+    headline TEXT,
     url TEXT,
     paper TEXT,
-    timestamp TEXT
+    timestamp TEXT,
+    paper_id TEXT
     );"""
-INSERT_HEADLINE = "INSERT INTO headlines (headline, url, paper, timestamp) VALUES (%s, %s, %s, %s);"
+INSERT_HEADLINE = "INSERT INTO headlines (headline, url, paper, timestamp, paper_id) VALUES (%s, %s, %s, %s, %s);"
 SELECT_ALL_HEADLINES = "SELECT * FROM headlines;"
 
 # https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-add-column/
@@ -15,17 +18,21 @@ ADD_COLUMNS = """ALTER TABLE headlines
 ADD COLUMN id SERIAL
 ;"""
 
-# ADD_UNIQUE = "ALTER TABLE headlines ADD CONSTRAINT constraintname UNIQUE (headline)"
+ADD_UNIQUE = "ALTER TABLE headlines ADD CONSTRAINT constraintname UNIQUE (headline)"
+def drop_tables(connection):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(DROP_TABLE)
 
 def create_tables(connection):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(CREATE_HEADLINES)
 
-# def add_unique(connection):
-#     with connection:
-#         with connection.cursor() as cursor:
-#             cursor.execute(ADD_UNIQUE)
+def add_unique(connection):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(ADD_UNIQUE)
 
 def add_columns(connection):
     with connection:
@@ -33,10 +40,10 @@ def add_columns(connection):
             cursor.execute(ADD_COLUMNS)
 
 
-def add_headline(connection, headline, url, paper, timestamp):
+def add_headline(connection, headline, url, paper, timestamp, paper_id):
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(INSERT_HEADLINE, (headline, url, paper, timestamp))        
+            cursor.execute(INSERT_HEADLINE, (headline, url, paper, timestamp, paper_id))        
 
 def get_headlines(connection):
     with connection:
